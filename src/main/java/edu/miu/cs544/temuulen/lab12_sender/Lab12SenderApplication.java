3cbb8a19ff571e4b24cdb1d5ead812925ff30132
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-13 21:25:30 -0600

    Initial commit

---
+++ src/main/java/edu/miu/cs544/temuulen/lab12_sender/Lab12SenderApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.temuulen.lab12_sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab12SenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab12SenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs544/temuulen/lab12_sender/Lab12SenderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs544.temuulen.lab12_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab12SenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
